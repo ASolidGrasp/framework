634f3ed6a811c0a18735b813e6c6cf430d4f6f32
Filippo Guenzi <filippoguenzi@hotmail.com>
2015-10-01 08:53:10 +0200

    Dernier commit avant livraison

--- src/main/java/com/formation/controller/ServletController.java
+++ src/main/java/com/formation/controller/ServletController.java
@@ -81,6 +81,7 @@ public class ServletController extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        doPost(request, response);
     }
 
     @Override
